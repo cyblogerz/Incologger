7811210818d8428cd4a0698f5555f50771382708
Pranav Ajay <pranavajay2002@gmail.com>
2021-12-09 11:45:57 +0530

    Changed blog element layout

--- lib/components/blog_element.dart
+++ lib/components/blog_element.dart
@@ -15,72 +15,89 @@ class BlogElement extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {Navigator.push(context, MaterialPageRoute(builder:(context) => BlogView(title: title) ));},
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                 height: 100,
-                width:100,
-                decoration: BoxDecoration(color: Colors.teal,
-                borderRadius: BorderRadius.circular(20)
-               
-                ),
-                
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-          Expanded(
-            child: Container(
-              height: 100,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              
-                
-                children: <Widget>[
-              
-                  
-                  Text(title),
-                  Text(date),
-                  Padding(
-                    padding: const EdgeInsets.only(right:10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Row(
-                          
-                          children: <Widget>[
-                          Text(likes.toString()),
-                          Padding(
-                            padding: const EdgeInsets.only(left:8.0,bottom: 2.0),
-                            child: Icon(Icons.thumb_up),
-                          )
-                        ],),
-                        Padding(
-                          padding: const EdgeInsets.only(right:50.0),
-                          child: Row(children: <Widget>[
-                            Text(comments.toString()),
-                            Padding(
-                              padding: const EdgeInsets.only(left:8.0),
-                              child: Icon(Icons.comment),
-                            ),
-                            
-                          ],),
-                        ),
-                        
-                      ],
-                    ),
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+      BoxShadow(
+        color: Colors.grey.withOpacity(0.2),
+        spreadRadius: 1,
+        blurRadius: 10,
+        offset: Offset(0,10), // changes position of shadow
+      ),
+    ],
+
+          color: Colors.white,
+
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                   height: 100,
+                  width:75,
+                  decoration: BoxDecoration(color: Colors.teal,
+                  borderRadius: BorderRadius.circular(15)
+                 
                   ),
-                ],
+                  
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+            Expanded(
+              child: Container(
+                height: 100,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                
+                  
+                  children: <Widget>[
+                
+                    
+                    Text(title),
+                    Text(date),
+                    Padding(
+                      padding: const EdgeInsets.only(right:10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Row(
+                            
+                            children: <Widget>[
+                            Text(likes.toString()),
+                            Padding(
+                              padding: const EdgeInsets.only(left:8.0,bottom: 2.0),
+                              child: Icon(Icons.thumb_up),
+                            )
+                          ],),
+                          Padding(
+                            padding: const EdgeInsets.only(right:50.0),
+                            child: Row(children: <Widget>[
+                              Text(comments.toString()),
+                              Padding(
+                                padding: const EdgeInsets.only(left:8.0),
+                                child: Icon(Icons.comment),
+                              ),
+                              
+                            ],),
+                          ),
+                          
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
+            ],
           ),
-          ],
         ),
       ),
     );
