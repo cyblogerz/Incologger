f4344eb0852e85757fa0fd24f82166c8ff5d4006
Pranav Ajay <pranavajay2002@gmail.com>
2021-12-13 16:52:21 +0530

    Updated featured element

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -47,29 +47,26 @@ class Home extends StatelessWidget {
               
                 Stack(children: <Widget>[
                   Container(
+                    alignment: Alignment.bottomLeft,
                     height: MediaQuery.of(context).size.height*0.50,
                     width: double.infinity,
                     decoration: BoxDecoration(
                       color: Colors.red,
                       borderRadius: BorderRadius.circular(32),
                     ),
-                    child: Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Column(
-                    children:<Widget>[ Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text("Here is what you need to know about DigiCupid",
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold
-                      ),),
-                      
-                    ),
                     
+                      
+                        child: Padding(
+                          padding: const EdgeInsets.only(left:32.0,right: 32.0,bottom: 75.0),
+                          child: Text("Here is what you need to know about DigiCupid",
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold
+                          ),),
+                          
+                        ),
+                      ),
                   
-                    ]
-                  )),
-                  ),
                   
                    Align(
                     alignment: Alignment.topRight,
@@ -79,6 +76,50 @@ class Home extends StatelessWidget {
                         primary: Colors.black,
                       ),
                     ))),
+
+                    Positioned(
+                      bottom: 0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(32.0),
+                        child: Row(
+                          
+                          children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right:16),
+                          child: CircleAvatar(backgroundColor: Colors.black,radius: 12,),
+                        ),
+                        Row(
+                          
+                            children: <Widget>[
+                              Text("Pranav Ajay"),
+                              Text("."),
+                              Text("6 min read"),
+                              Text("."),
+                              Padding(
+                                padding: const EdgeInsets.only(left:8.0),
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(horizontal:10,vertical: 3),
+                                  decoration: BoxDecoration(
+                                    color:Colors.grey,
+                                    borderRadius: BorderRadius.circular(10),
+
+                                  ),
+                                  child: Text("Technology"),
+                                ),
+                              ),
+
+                              
+                        
+                            ],
+                            
+                          ),
+
+
+                        
+                    ],),
+                      ),),
+
+                  
                   
                 ]),
                  
